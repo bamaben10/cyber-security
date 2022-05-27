6dfbdb71fcf5853a87abcb35a2b956b0ee923acf
Benjamin Salmon <bensalmon8@gmail.com>
2022-05-27 08:23:15 -0400

    initial commit

---
+++ part-1/server/controllers/auth.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs')
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      console.log('Logging In User')
+      console.log(req.body)
+      const { username, password } = req.body
+      
+      for (let i = 0; i < users.length; i++) {
+        const matching = bcrypt.compareSync(password, users[i].passwordHash);
+        if (matching) {
+          let userCopy = user[i];
+          delete userCopy.passwordHash;
+          res.status(200).send(userCopy);
+          console.log(userCopy);
+          return;
+        }
+      }
+      res.status(400).send("User not found.");
+    },
+    
+    register: (req, res) => {
+        console.log('Registering User')
+        console.log(req.body)
+        const { username, email, firstName, lastName, password } = req.body
+        const salt = bcrypt.genSaltSync(5)
+        const passwordHash = bcrypt.hashSync(password, salt)
+        let user = {
+          username,
+          email,
+          firstName,
+          lastName,
+          passwordHash
+        }
+
+        users.push(user)
+        console.log(users)
+        res.status(200).send(req.body)
+    }
+}
